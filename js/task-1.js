03789ae39b707eb6310ba1beb42a7331a40bee81
Timofii <pobaranchuktima@gmail.com>
2025-08-07 17:13:44 +0300

    all

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const makeTransaction = (quantity, pricePerDroid) => `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
